6bb5beb695c4cdc3900e502889ce328d348f5966
Vivek Kumar <VK285090@GMAIL.COM>
2023-01-21 17:49:05 +0530

    add

--- crop functions/raw code/app.js
+++ crop functions/raw code/app.js
@@ -2,48 +2,63 @@
 const cropImage = document.getElementById("cropImage");
 const cropModal = document.querySelector(".crop-modal");
 const saveBtn = document.querySelector(".saveBtn");
-const savedImage = document.querySelector(".savedImage");
-const inputImage = document.getElementById("input-image");
+const savedImage0 = document.querySelector(".savedImage-0");
+
+
+const savedImage1 = document.querySelector(".savedImage-1");
+const inputImage0 = document.getElementById("input-image-0");
+const inputImage1 = document.getElementById("input-image-1");
 const showImage = document.querySelector(".show-image");
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
 
 
 let cropper = "";
-
-
-inputImage.addEventListener("change", (e)=> {
-    console.log("Hello world");
-  const reader = new FileReader();
+inputs.forEach((input)=>{
+  input.addEventListener("change",(e)=>{
+    let id = (e.target.id);
+    let uniqueString = id.charAt(id.length -1);
+    const imageFileName = document.getElementsByClassName(`savedImage-${uniqueString}`);
+    console.log(imageFileName);
+  })
   
-reader.onload = (e)=>{
-    let image = document.createElement("img");
-    image.id = "image";
-    image.src = e.target.result;
-    cropImage.innerHTML = '';
-    console.log(cropImage);
-    cropImage.appendChild(image);
+})
+
+// InputElement.addEventListener("change", (e)=> {
+//     console.log("Hello world");
+//   const reader = new FileReader();
+  
+// reader.onload = (e)=>{
+//     let image = document.createElement("img");
+//     image.id = "image";
+//     image.src = e.target.result;
+//     cropImage.innerHTML = '';
+//     console.log(cropImage);
+//     cropImage.appendChild(image);
     
-    cropModal.classList.remove("hide");
+//     cropModal.classList.remove("hide");
     
-    cropper = new Cropper(image,{
+//     cropper = new Cropper(image,{
                     
-        viewMode: 1
-    });
-}  
-  reader.readAsDataURL(e.target.files[0]);
-});
+//         viewMode: 1
+//     });
+// }  
+//   reader.readAsDataURL(e.target.files[0]);
+// });
 
 
-saveBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+// saveBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  let imageSource = cropper
-    .getCroppedCanvas({
-      width: '300',
-    })
-    .toDataURL();
+//   let imageSource = cropper
+//     .getCroppedCanvas({
+//       width: '300',
+//     })
+//     .toDataURL();
 
-    showImage.classList.remove("hide");
-    savedImage.src = imageSource;
+//     showImage.classList.remove("hide");
+//     savedImage.src = imageSource;
 
     
-});
+// });
