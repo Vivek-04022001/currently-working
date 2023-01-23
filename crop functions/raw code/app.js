ee19520081885590d1ed091fd3d2eadb324c6fa4
Vivek Kumar <VK285090@GMAIL.COM>
2023-01-23 21:37:12 +0530

    add

--- crop functions/raw code/app.js
+++ crop functions/raw code/app.js
@@ -4,59 +4,72 @@ const cropModal = document.querySelector(".crop-modal");
 const saveBtn = document.querySelector(".saveBtn");
 const savedImage0 = document.querySelector(".savedImage-0");
 
-
 const savedImage1 = document.querySelector(".savedImage-1");
 const inputImage0 = document.getElementById("input-image-0");
 const inputImage1 = document.getElementById("input-image-1");
-const showImage = document.querySelector(".show-image");
+// const showImage = document.querySelector(".show-image");
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-
+const inputs = document.querySelectorAll("input");
+// console.log(inputs);
 
 let cropper = "";
-inputs.forEach((input)=>{
-  input.addEventListener("change",(e)=>{
-    let id = (e.target.id);
-    let uniqueString = id.charAt(id.length -1);
-    const imageFileName = document.getElementsByClassName(`savedImage-${uniqueString}`);
-    console.log(imageFileName);
+inputs.forEach((input) => {
+  input.addEventListener("change", (e) => {
+    let id = e.target.id;
+    let uniqueString = id.charAt(id.length - 1);
+    const imageFileName = document.getElementsByClassName(
+      `savedImage-${uniqueString}`
+    );
+    // console.log(imageFileName);
 
     const reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
-   reader.onload = (e)=>{
-    let image = document.createElement("img");
-    // image.id = "image";
-    image.id = `image-${uniqueString}`
-    console.log(image.id);
-    image.src = e.target.result;
-    cropImage.innerHTML = '';
-    console.log(cropImage);
-    cropImage.appendChild(image);
-    
-    cropModal.classList.remove("hide");
-    
-    cropper = new Cropper(image,{
-                    
-        viewMode: 1
-    });
-} 
+    reader.onload = (e) => {
+      let image = document.createElement("img");
+      // image.id = "image";
+      image.id = `image-${uniqueString}`;
+      // console.log(image.id);
+      image.src = e.target.result;
+      cropImage.innerHTML = "";
+      // console.log(cropImage);
+      cropImage.appendChild(image);
 
-  })
+      cropModal.classList.remove("hide");
 
-  saveBtn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log(document.getElementsByClassName('cropper-hidden'));
-  })
-  
-})
+      cropper = new Cropper(image, {
+        viewMode: 1,
+      });
+    };
+  });
+});
+
+saveBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const Id = document.getElementById("cropImage").querySelector("img").id;
+  const uniqueId = Id.charAt(Id.length - 1);
+  const savedImageSource = document.getElementsByClassName(
+    `savedImage-${uniqueId}`
+  );
+  console.log(savedImageSource[0]);
+
+  const imageSource = cropper
+    .getCroppedCanvas({
+      width: "300",
+    })
+    .toDataURL();
+
+    const showImage = document.getElementsByClassName(`show-image-${uniqueId}`)[0];
+    showImage.classList.remove("hide");
+
+    savedImageSource[0].src = imageSource ;
 
 
+});
 
 // InputElement.addEventListener("change", (e)=> {
 //     console.log("Hello world");
 //   const reader = new FileReader();
-  
+
 // reader.onload = (e)=>{
 //     let image = document.createElement("img");
 //     image.id = "image";
@@ -64,17 +77,16 @@ inputs.forEach((input)=>{
 //     cropImage.innerHTML = '';
 //     console.log(cropImage);
 //     cropImage.appendChild(image);
-    
+
 //     cropModal.classList.remove("hide");
-    
+
 //     cropper = new Cropper(image,{
-                    
+
 //         viewMode: 1
 //     });
-// }  
+// }
 //   reader.readAsDataURL(e.target.files[0]);
 // });
-
 
 // saveBtn.addEventListener("click", function (e) {
 //   e.preventDefault();
@@ -88,5 +100,4 @@ inputs.forEach((input)=>{
 //     showImage.classList.remove("hide");
 //     savedImage.src = imageSource;
 
-    
 // });
