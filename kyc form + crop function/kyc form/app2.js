63a67aa7f4d6c2d6238148a62ab6e53e666b69a6
Vivek Kumar <VK285090@GMAIL.COM>
2023-01-25 00:15:06 +0530

    add

--- kyc form + crop function/kyc form/app2.js
+++ kyc form + crop function/kyc form/app2.js
@@ -1,32 +1,29 @@
 const labels = document.querySelectorAll('.container-button label');
-console.log(labels);
+const body = document.body;
 
-labels.forEach((label)=>{
-    label.addEventListener('click',function(e){
-        e.preventDefault();
-        openModal();
-        
-    })
+// event handler
+labels.forEach(label=>{
+  label.addEventListener('click',function(e){
+    console.log('clicked');
+    e.preventDefault();
+    openModal();
+  })
 })
 
 
-
-// /---------------------------------------------------------------
-// /---------------------------------------------------------------
-// /---------------------------------------------------------------
-
+// ---------------------------------------
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-const body =
 console.log(btnsOpenModal);
 
 const openModal = function () {
   console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  body.classList.add('stop-scroll')
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
@@ -36,7 +33,8 @@ const closeModal = function () {
   console.log('close button clicked');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-  document.body.classList.remove('stop-scroll');
+  body.classList.remove('stop-scroll')
+
 };
 
 btnCloseModal.addEventListener('click', closeModal);
