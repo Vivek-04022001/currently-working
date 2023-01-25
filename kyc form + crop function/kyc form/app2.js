52b27a5105759be13320bf97abfdf61e5cbde302
Vivek Kumar <VK285090@GMAIL.COM>
2023-01-25 23:29:43 +0530

    cut css

--- kyc form + crop function/kyc form/app2.js
+++ kyc form + crop function/kyc form/app2.js
@@ -14,7 +14,7 @@ const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
   body.classList.add("stop-scroll");
-  modal.classList.add('position-fixed');
+  
 };
 
 const closeModal = function () {
@@ -22,7 +22,7 @@ const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
   body.classList.remove("stop-scroll");
-  modal.classList.remove('position-fixed');
+  
 
 };
 
@@ -39,23 +39,23 @@ inputs.forEach((input) => {
     const inputIdUniqueNumber = inputId.charAt(inputId.length -1);
 
     // create reader object
-    // const reader = new FileReader();
-    // reader.readAsDataURL(e.target.files[0]);
-    // reader.onload = function(e){
-    //   let image = document.createElement('img');
-    //   image.id = `image-${inputIdUniqueNumber}`;
-    //   image.src = e.target.result;
-    //   console.log(image.id);
-    //   console.log(image.src);
-    //   //clear cropImage
-    //   cropImage.innerHTML = '';
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = function(e){
+      let image = document.createElement('img');
+      image.id = `image-${inputIdUniqueNumber}`;
+      image.src = e.target.result;
+      console.log(image.id);
+      console.log(image.src);
+      //clear cropImage
+      cropImage.innerHTML = '';
 
-    //   cropImage.appendChild(image);
+      cropImage.appendChild(image);
 
-    //   cropper = new cropper(image,{
-    //     viewMode: 1,
-    //   });
-    // };
+      cropper = new cropper(image,{
+        viewMode: 1,
+      });
+    };
     
     
   });
